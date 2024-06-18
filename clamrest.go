869695f8f5e17160257795c3671281c6afeb64f2
Erik Svensson <arizon.es@gmail.com>
2024-06-18 22:43:12 +0200

    fix response for multi file stream scanning so that it doesn't return the first file's status regardless of the next files. It now returns the most severe status (virus detected) and only 200OK if all files are clean

--- clamrest.go
+++ clamrest.go
@@ -122,11 +122,16 @@ func v2ScanHandler(w http.ResponseWriter, r *http.Request) {
 	scanner(w, r, 2)
 }
 
-// This is where the action happens.
+// old endpoint version, set deprecation header to indicate usage of the new /v2/scan
 func scanHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Deprecation", "version=v1")
+	v2url := fmt.Sprintf("%s%s/v2/scan", string(r.URL.Scheme), r.Host)
+	w.Header().Add("Link", fmt.Sprintf("%v; rel=successor-version", v2url))
+
 	scanner(w, r, 1)
 }
 
+// This is where the action happens.
 func scanner(w http.ResponseWriter, r *http.Request, version int) {
 	switch r.Method {
 	//POST takes the uploaded file(s) and saves it to disk.
@@ -139,8 +144,8 @@ func scanner(w http.ResponseWriter, r *http.Request, version int) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
 		//copy each part to destination.
+		resp := []scanResponse{}
 		for {
 			part, err := reader.NextPart()
 			if err == io.EOF {
@@ -150,9 +155,9 @@ func scanner(w http.ResponseWriter, r *http.Request, version int) {
 			//if part.FileName() is empty, skip this iteration.
 			if part.FileName() == "" {
 				if version == 2 {
-					respJson := "{ \"Status\": \"ERROR\", \"Description\": \"MimePart FileName missing!\" }"
+					fileResp := scanResponse{Status: "ERROR", Description: "MimePart FileName missing"}
+					resp = append(resp, fileResp)
 					w.WriteHeader(http.StatusUnprocessableEntity)
-					fmt.Fprint(w, respJson)
 					fmt.Printf("%v Not scanning, MimePart FileName not supplied\n", time.Now().Format(time.RFC3339))
 				}
 				continue
@@ -162,45 +167,89 @@ func scanner(w http.ResponseWriter, r *http.Request, version int) {
 			var abort chan bool
 			response, err := c.ScanStream(part, abort)
 			if err != nil {
-				fmt.Printf("%v Clamd returned an error, %v\n", time.Now().Format(time.RFC3339), err)
-				w.WriteHeader(http.StatusRequestEntityTooLarge)
-				respJson := fmt.Sprintf("{ \"Status\": \"%s\", \"Description\": \"%s\" }",
-					clamd.RES_PARSE_ERROR, fmt.Sprintf("Scanning aborted, the underlying process returned an error, %v\n", err))
-				w.Write([]byte(respJson))
-				continue
+				//error occurred, response is nil, create a custom response and send it on the channel to handle it together with the other errors.
+				response = make(chan *clamd.ScanResult)
+				scanErrResult := &clamd.ScanResult{Status: clamd.RES_PARSE_ERROR, Description: "File size limit exceeded"}
+				go func() {
+					response <- scanErrResult
+					close(response)
+					fmt.Printf("%v Clamd returned an error, probably a too large file as input (causing broken pipe and closed connection) %v\n", time.Now().Format(time.RFC3339), err)
+					//The underlying service closes the connection if the file is to large, logging output
+					//We never receive the clamd output of `^INSTREAM: Size limit reached` up here, just a closed connection.
+				}()
+
 			}
 			for s := range response {
 				w.Header().Set("Content-Type", "application/json; charset=utf-8")
-				respJson := fmt.Sprintf("{ \"Status\": \"%s\", \"Description\": \"%s\" }", s.Status, s.Description)
+				eachResp := scanResponse{Status: s.Status, Description: s.Description}
+				//Set each possible status and then send the most appropriate one
 				switch s.Status {
 				case clamd.RES_OK:
-					w.WriteHeader(http.StatusOK)
+					eachResp.httpStatus = 200
 				case clamd.RES_FOUND:
-					w.WriteHeader(http.StatusNotAcceptable)
+					eachResp.httpStatus = 406
 					if version == 2 {
 						fmt.Printf("%v Virus FOUND, filename: %v\n", time.Now().Format(time.RFC3339), part.FileName())
 						noOfFoundViruses.Inc()
 					}
 				case clamd.RES_ERROR:
-					w.WriteHeader(http.StatusBadRequest)
+					eachResp.httpStatus = 400
 				case clamd.RES_PARSE_ERROR:
-					w.WriteHeader(http.StatusPreconditionFailed)
+					if s.Description == "File size limit exceeded" {
+						eachResp.httpStatus = 413
+					} else {
+						eachResp.httpStatus = 412
+					}
 				default:
-					w.WriteHeader(http.StatusNotImplemented)
+					eachResp.httpStatus = 501
 				}
-				if err != nil {
-					fmt.Printf("received error from clamd, %v", err)
-					w.WriteHeader(http.StatusInternalServerError)
-				}
-				fmt.Fprint(w, respJson)
+				resp = append(resp, eachResp)
 				fmt.Printf("%v Scan result for: %v, %v\n", time.Now().Format(time.RFC3339), part.FileName(), s)
 			}
 			fmt.Printf("%v Finished scanning: %v\n", time.Now().Format(time.RFC3339), part.FileName())
+		}
+		w.WriteHeader(getResponseStatus(resp))
+		if version == 2 {
+			jsonRes, jErr := json.Marshal(resp)
+			if jErr != nil {
+				fmt.Printf("Error marshalling json, %v\n", jErr)
+			}
+			fmt.Fprint(w, string(jsonRes))
+		} else {
+			for _, v := range resp {
+				jsonRes, jErr := json.Marshal(v)
+				if jErr != nil {
+					fmt.Printf("Error marshalling json, %v\n", jErr)
+				}
+				fmt.Fprint(w, string(jsonRes))
+			}
 		}
 
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
+}
+
+// this func returns 406 if one file contains a virus, 413 if one file wasn't scanable due to file size exceeded (closed connection) and otherwise 200 OK.
+func getResponseStatus(responses []scanResponse) int {
+	result := 200
+	for _, s := range responses {
+		switch s.httpStatus {
+		case 406:
+			//early return if virus is found
+			return 406
+		case 400:
+			result = 400
+		case 412:
+			result = 412
+		case 413:
+			result = 413
+		case 501:
+			result = 501
+		}
+	}
+
+	return result
 }
 
 func scanHandlerBody(w http.ResponseWriter, r *http.Request) {
@@ -211,12 +260,24 @@ func scanHandlerBody(w http.ResponseWriter, r *http.Request) {
 
 	c := clamd.NewClamd(opts["CLAMD_PORT"])
 
-	fmt.Printf(time.Now().Format(time.RFC3339) + " Started scanning plain body\n")
+	fmt.Printf("%v Started scanning plain body\n", time.Now().Format(time.RFC3339))
 	var abort chan bool
 	defer r.Body.Close()
-	response, _ := c.ScanStream(r.Body, abort)
+	response, err := c.ScanStream(r.Body, abort)
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	if err != nil {
+		w.WriteHeader(http.StatusRequestEntityTooLarge)
+		resp := scanResponse{Status: clamd.RES_PARSE_ERROR, Description: "File size limit exceeded"}
+		fmt.Printf("%v Clamd returned error, broken pipe and closed connection can indicate too large file, %v", time.Now().Format(time.RFC3339), err)
+		jsonResp, jErr := json.Marshal(resp)
+		if jErr != nil {
+			fmt.Printf("%v Error marshalling json, %v", time.Now().Format(time.RFC3339), jErr)
+		}
+		fmt.Fprint(w, string(jsonResp))
+		return
+	}
 	for s := range response {
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		respJson := fmt.Sprintf("{ Status: %q, Description: %q }", s.Status, s.Description)
 		switch s.Status {
 		case clamd.RES_OK:
