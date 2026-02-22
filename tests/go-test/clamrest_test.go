67c8c0a8aa5b0c467184baf31a21e794d4456d11
Erik Svensson <arizon.es@gmail.com>
2026-02-22 17:39:15 +0100

    style: rename baseUrl to baseURL

--- tests/go-test/clamrest_test.go
+++ tests/go-test/clamrest_test.go
@@ -9,23 +9,37 @@ import (
 )
 
 var (
-	baseUrl = url.URL{
+	baseURL = url.URL{
 		Scheme: "http",
 		Host:   "localhost:9000",
 	}
 	c = http.Client{}
 )
 
-func TestScanFileHandler_nonVirus(t *testing.T) {
-	res, err := c.Get(baseUrl.String() + "/scanFile?path=/clamav/tmp/ok/test.txt")
+// Test scanFileHandler, should return 200
+func TestScanFileHandler_NonVirus(t *testing.T) {
+	res, err := c.Get(baseURL.String() + "/scanFile?path=/clamav/tmp/ok/test.txt")
 	assert.NoError(t, err)
 	assert.Equal(t, http.StatusOK, res.StatusCode)
-	assert.Error(t, err)
 }
 
+// Test scanFileHandler, should return 406
 func TestScanFileHAndler_WithVirus(t *testing.T) {
-	res, err := c.Get(baseUrl.String() + "/scanFile?path=/clamav/tmp/virus/eicar.test")
+	res, err := c.Get(baseURL.String() + "/scanFile?path=/clamav/tmp/virus/eicar.test")
 	assert.NoError(t, err)
 	assert.Equal(t, http.StatusNotAcceptable, res.StatusCode)
-	assert.Error(t, err)
+}
+
+// Test scanPathHandler, should return 200
+func TestScanPathHandler_NonVirus(t *testing.T) {
+	res, err := c.Get(baseURL.String() + "/scanPath?path=/clamav/tmp/ok")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+}
+
+// Test scanPathHandler, should return 406
+func TestScanPathHandler_WithVirus(t *testing.T) {
+	res, err := c.Get(baseURL.String() + "/scanPath?path=/clamav/tmp/virus")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusNotAcceptable, res.StatusCode)
 }
