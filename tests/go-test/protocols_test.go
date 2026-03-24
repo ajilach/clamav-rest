4f776c2c0c678f9ace1a18d9f00955bcbc7df5cb
Erik Svensson <arizon.es@gmail.com>
2026-03-24 22:30:27 +0100

    test: Add test for HTTP/2 over TLS
    and rename file

--- tests/go-test/protocols_test.go
+++ tests/go-test/protocols_test.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"testing"
 
@@ -19,7 +20,7 @@ func TestHTTP1_1(t *testing.T) {
 	ts.Protocols.SetHTTP1(true)
 	ts.Protocols.SetHTTP2(false)
 
-	resp, err := performCall(ts, nil)
+	resp, err := performCall(ts, nil, nil)
 	if err != nil {
 		t.Errorf("TestHTTP1_1 failed, %v", err)
 	}
@@ -43,17 +44,23 @@ func TestHTTP1_1(t *testing.T) {
 // Test h2c, HTTP/2 over HTTP (non-tls) with Prior knowledge
 func TestH2C(t *testing.T) {
 	// h2c is not implemented in the stdlib http.Transport, if *http.Transport.Protocols.SetHTTP2(true) and SetHTTP1(false) is used, HTTP/2 over TLS will be attempted, or it will fall back to HTTP/1.1 over non-TLS HTTP.
-	ts := &http2.Transport{
-		AllowHTTP: true,
-		DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
-			return net.Dial(network, addr)
-		},
+	//ts := &http2.Transport{
+	//	AllowHTTP: true,
+	//	DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
+	//		return net.Dial(network, addr)
+	//	},
+	//}
+	tsp := http.DefaultTransport.(*http.Transport).Clone()
+	tsp.Protocols = new(http.Protocols)
+	tsp.Protocols.SetUnencryptedHTTP2(true)
+	if !tsp.Protocols.UnencryptedHTTP2() {
+		t.Errorf("TestH2C failed, tried to set H2C but failed")
 	}
-
-	res, err := performCall(nil, ts)
+	res, err := performCall(tsp, nil, nil)
 	if err != nil {
 		t.Errorf("TestHTTP2 failed, %v", err)
 	}
+
 	gotScheme := res.Request.URL.Scheme
 	wantScheme := "http"
 	wantProtoMajor := 2
@@ -65,10 +72,34 @@ func TestH2C(t *testing.T) {
 
 // Test to check that HTTP/2 works over TLS
 func TestHTTP2_TLS(t *testing.T) {
-	t.Errorf("TestHTTP2_TLS not implemented")
+	ts := http.DefaultTransport.(*http.Transport).Clone()
+	ts.Protocols = new(http.Protocols)
+	ts.Protocols.SetHTTP2(true)
+	ts.TLSClientConfig = &tls.Config{
+		MinVersion:         tls.VersionTLS12,
+		InsecureSkipVerify: true,
+	}
+	url, err := getURL(nil, "v2", "scan")
+	if err != nil {
+		t.Errorf("TestHTTP2_TLS failed when creating url, %v", err)
+	}
+	url.Scheme = "https"
+	url.Host = net.JoinHostPort(url.Hostname(), "9443")
+	res, err := performCall(ts, nil, url)
+	if err != nil {
+		t.Errorf("TestHTTP2_TLS failed, %v", err)
+	}
+
+	gotScheme := res.Request.URL.Scheme
+	wantScheme := "https"
+	wantProto := 2
+	gotProto := res.ProtoMajor
+	if gotProto != wantProto || gotScheme != wantScheme {
+		t.Errorf("TestHTTP2_TLS failed, wanted scheme %v, got %v, wanted major protocol version %d, got major protocol version %d", wantScheme, gotScheme, wantProto, gotProto)
+	}
 }
 
-func performCall(ts *http.Transport, ts2 *http2.Transport) (*http.Response, error) {
+func performCall(ts *http.Transport, ts2 *http2.Transport, url *url.URL) (*http.Response, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
 		return nil, err
@@ -78,8 +109,16 @@ func performCall(ts *http.Transport, ts2 *http2.Transport) (*http.Response, erro
 	if err != nil {
 		return nil, err
 	}
-	u, err := getURL(nil, "v2", "scan")
-	req.URL = u
+	if url == nil {
+		u, err := getURL(nil, "v2", "scan")
+		if err != nil {
+			return nil, err
+		}
+		req.URL = u
+	} else {
+		req.URL = url
+	}
+
 	if err != nil {
 		return nil, err
 	}
